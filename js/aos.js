dc88abd1fda65ddbcd018de9a30f19b9983be880
spinnning <maximilian.hilmbauer@sop.co.at>
2023-10-07 10:05:00 +0200

    added reveal and aos

---
+++ js/aos.js
@@ -0,0 +1,8 @@
+// Import AOS
+import AOS from "aos";
+
+// Initialize AOS
+AOS.init({
+  duration: 800, // Duration of animations (in milliseconds)
+  once: true, // Only animate elements once
+});
---
+++ js/reveal.js
@@ -0,0 +1,3 @@
+import ScrollReveal from "scrollreveal";
+
+ScrollReveal().reveal("main", { delay: 100 });
